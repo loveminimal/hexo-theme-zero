b6cb0bcc1929491174912c5eb6357fdb09c232ea
Jack <loveminimal@163.com>
2019-01-29 16:52:47 +0800

    A new way to sroll top.

--- source/js/totop.js
+++ source/js/totop.js
@@ -3,5 +3,6 @@
 let atc = document.querySelector('.article');
 // atc.setAttribute('ondblclick', 'toTop()');
 atc.ondblclick = function() {
-    document.documentElement.scrollTop = 0;
+    // document.documentElement.scrollTop = 0;
+    window.scrollTo({top: 0, behavior: 'auto'});    // instant, smooth, auto
 }
